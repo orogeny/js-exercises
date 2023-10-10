3648b5279bf5f99ca9977150ee3b3b7adc2c8101
Paul <paulwharin@gmail.com>
2023-10-10 13:20:08 +0100

    feat: implement exercise2 functions

--- challenges/exercise2.js
+++ challenges/exercise2.js
@@ -1,26 +1,39 @@
 export function getSquares(nums) {
 	if (nums === undefined) throw new Error('nums is required');
-	// Your code here!
+
+	return nums.map(x => x * x);
 }
 
 export function camelCaseWords(words) {
 	if (words === undefined) throw new Error('words is required');
-	// Your code here!
+
+	return words.reduce((acc, x) => acc + x.charAt(0).toUpperCase() + x.substring(1));
 }
 
 export function getTotalSubjects(people) {
 	if (people === undefined) throw new Error('people is required');
-	// Your code here!
+
+	return people.reduce((acc, x) => (acc += x.subjects.length), 0);
 }
 
 export function checkIngredients(menu, ingredient) {
 	if (menu === undefined) throw new Error('menu is required');
 	if (!ingredient) throw new Error('ingredient is required');
-	// Your code here!
+
+	return menu.some(x => x.ingredients.includes(ingredient));
 }
 
 export function duplicateNumbers(arr1, arr2) {
 	if (arr1 === undefined) throw new Error('arr1 is required');
 	if (arr2 === undefined) throw new Error('arr2 is required');
-	// Your code here!
+
+	const intersection = new Set();
+
+	for (const a of arr1) {
+		if (arr2.includes(a)) {
+			intersection.add(a);
+		}
+	}
+
+	return Array.from(intersection).sort((a, b) => a - b);
 }
